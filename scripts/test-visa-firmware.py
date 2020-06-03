b7a606375be9e832e47b777620c437eed54a218e
David Fokkema <davidfokkema@gmail.com>
2020-06-03 21:48:37 +0200

    Implemented DAC write (and read) commands

--- scripts/test-visa-firmware.py
+++ scripts/test-visa-firmware.py
@@ -1,4 +1,6 @@
 import pyvisa
+import time
+
 
 rm = pyvisa.ResourceManager("@py")
 dev_str = rm.list_resources()[-1]
@@ -6,5 +8,14 @@ dev = rm.open_resource(dev_str)
 dev.write_termination = '\n'
 dev.read_termination = '\r\n'
 
+
 print(dev.query("*IDN?"))
-print(dev.query("OUT:CH1 434"))
+print(dev.query("OUT:CH1 511"))
+time.sleep(.5)
+print(dev.query("OUT:CH1 767"))
+time.sleep(.5)
+print(dev.query("OUT:CH1 1023"))
+time.sleep(1)
+print(dev.query("OUT:CH1?"))
+print(dev.query("OUT:CH1 0"))
+print(dev.query("OUT:CH1?"))
--- visa_firmware/visa_firmware.ino
+++ visa_firmware/visa_firmware.ino
@@ -1,19 +1,35 @@
 #include <Regexp.h>
 
-#define COMM_IDN    "*IDN?"
-#define COMM_DAC    "OUT:CH"
+#define DAC_BITS      10
 
-#define IDN_STRING  "Arduino VISA firmware v0.1"
+#define COM_IDN       "*IDN?"
+#define COM_WRITE_DAC "^OUT:CH(%d) (%d+)$"
+#define COM_READ_DAC  "^OUT:CH(%d)?$"
 
-#define BUFFER_LENGTH  100
+#define IDN_STRING    "Arduino VISA firmware v0.1"
+
+#define BUFFER_LENGTH 100
 
 int DACchannel[] = {DAC0};
 int ADCchannel[] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
+#define MAX_DAC_CHANNEL sizeof(DACchannel)  / sizeof(int)
+#define MAX_ADC_CHANNEL sizeof(ADCchannel)  / sizeof(int)
+
+int DACvalues[MAX_DAC_CHANNEL];
+
 void setup() {
+  int i;
+  
   Serial.begin(9600);
   Serial.setTimeout(-1);
   Serial.flush();
+
+  analogWriteResolution(DAC_BITS);
+  for (i = 0; i < MAX_DAC_CHANNEL; i ++) {
+    analogWrite(DACchannel[i], 0);
+    DACvalues[i] = 0;
+  }
 }
 
 void loop() {
@@ -26,23 +42,22 @@ void loop() {
   msg.toCharArray(buffer, BUFFER_LENGTH);
   ms.Target(buffer);
 
-  if (msg == COMM_IDN) {
+  if (msg == COM_IDN) {
     Serial.println(IDN_STRING);
   }
-  else if (ms.Match("OUT:CH(%d) (%d+)") == 1) {
+  else if (ms.Match(COM_WRITE_DAC) == 1) {
     channel = atoi(ms.GetCapture(buffer, 0));
     value = atoi(ms.GetCapture(buffer, 1));
-    Serial.print("DAC channel ");
-    Serial.print(channel);
-    Serial.print(" value ");
+    analogWrite(DACchannel[channel - 1], value);
+    DACvalues[channel - 1] = value;
     Serial.println(value);
   }
-//  else if (msg.startsWith(COMM_DAC)) {
-//    channel = String(msg[sizeof(COMM_DAC) - 1]).toInt();
-////    println(analogWrite(DACchannel[channel]));
-//  }
+  else if (ms.Match(COM_READ_DAC) == 1) {
+    channel = atoi(ms.GetCapture(buffer, 0));
+    Serial.println(DACvalues[channel - 1]);
+  }
   else {
-    Serial.print("I have received: ");
+    Serial.print("UNKNOWN COMMAND: ");
     Serial.println(msg);
   }
 }
